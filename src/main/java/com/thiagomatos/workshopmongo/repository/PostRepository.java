226a67822ece2d8b553e290416c55d2f3ccffd1a
Thiago Fratucci de Matos <thi_fm@yahoo.com.br>
2023-05-17 16:46:50 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/thiagomatos/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.thiagomatos.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.thiagomatos.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
