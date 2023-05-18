9c90b26740d594efc83c5515e6bdd9b10b00fb6f
Thiago Fratucci de Matos <thi_fm@yahoo.com.br>
2023-05-18 09:53:01 -0300

    Obtendo um post por id

---
+++ src/main/java/com/thiagomatos/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.thiagomatos.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thiagomatos.workshopmongo.domain.Post;
+import com.thiagomatos.workshopmongo.repository.PostRepository;
+import com.thiagomatos.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
