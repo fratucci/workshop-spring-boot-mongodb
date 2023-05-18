ec59e5132be09f06835ee6ed018ca80a0fe7de1c
Thiago Fratucci de Matos <thi_fm@yahoo.com.br>
2023-05-18 09:38:42 -0300

    Referenciando os posts do usuário

--- src/main/java/com/thiagomatos/workshopmongo/domain/User.java
+++ src/main/java/com/thiagomatos/workshopmongo/domain/User.java
@@ -1,9 +1,12 @@
 package com.thiagomatos.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "user")
@@ -15,6 +18,9 @@ public class User implements Serializable {
 	private String name;
 	private String email;
 
+	@DBRef(lazy = true)
+	List<Post> posts = new ArrayList<>();
+	
 	public User() {
 	}
 
@@ -24,7 +30,6 @@ public class User implements Serializable {
 		this.name = name;
 		this.email = email;
 	}
-
 	public String getId() {
 		return id;
 	}
@@ -47,6 +52,14 @@ public class User implements Serializable {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	@Override
