eac270d0dbeb910feab49c5613c4b4fd383644f7
Thiago Fratucci de Matos <thi_fm@yahoo.com.br>
2023-05-18 11:27:33 -0300

    Consulta simples com @Query

--- src/main/java/com/thiagomatos/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/thiagomatos/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.thiagomatos.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.thiagomatos.workshopmongo.domain.Post;
@@ -10,5 +11,9 @@ import com.thiagomatos.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{ 'title': { $regex: ?0 , $options: 'i' } }")
+	List<Post> searchTitle(String text);
+
 	List<Post> findByTitleContainingIgnoreCase(String text);
+
 }
